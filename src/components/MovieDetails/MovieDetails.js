014917494996d5e1416acb6b468f043620b25901
Andrew Aiello <andrewaiello7@gmail.com>
2020-07-19 10:53:02 +1000

    added some more movie information

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -2,17 +2,44 @@ import React from 'react'
 
 const MovieDetails = ({ movieDetails }) => {
 
-    console.log(movieDetails)
-
     return (
-        <article id="movieDetails">
+        <section id="movieDetailsContainer">
             {
                 !movieDetails ?
                 <h1>No movie details found</h1>
                 :
-                <h1>{movieDetails.Title}</h1>
+                <article id="movieDetails">
+                    <h1>{movieDetails.Title}</h1>
+                    <article id="movieDetailsPoster">
+                        <figure>
+                            <img src={movieDetails.Poster} alt={movieDetails.Title}/>
+                        </figure>
+                        <article id="ratingsContainer">
+                            {
+                                movieDetails.Ratings.map((rating, i) => {
+                                    return (
+                                        <div key={i} className="rating">
+                                            <p>{rating.Source}</p>
+                                            <p>{rating.Value}</p>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </article>
+                    </article>
+                    <article id="plot">
+                        <p>{movieDetails.Plot}</p>
+                    </article>
+                    <article id="smallDetails">
+                        <p>{`Released: ${movieDetails.Released}`}</p>
+                        <p>{`Runtime: ${movieDetails.Runtime}`}</p>
+                        <p>{`Genre: ${movieDetails.Genre}`}</p>
+                        <p>{`Director: ${movieDetails.Director}`}</p>
+
+                    </article>
+                </article>
             }
-        </article>
+        </section>
     )
 }
 
